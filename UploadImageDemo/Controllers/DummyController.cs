ff1d8799d1d14478c70cb2e351c0d0d0b536aa37
Irshad Ahmed <Irshadahmed31@outlook.com>
2017-05-14 17:37:16 +0500

    Uploading Images/Files

---
+++ UploadImageDemo/Controllers/DummyController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UploadImageDemo.Controllers
+{
+    public class DummyController : Controller
+    {
+        // GET: Dummy
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Upload()
+        {
+            foreach (string f in Request.Files)
+            {
+                HttpPostedFileBase file = Request.Files[f];
+                if (file != null)
+                {
+                    string fileName = file.FileName;
+                    file.SaveAs(Request.MapPath("~/Content/Images/" + fileName));
+                }
+            }
+            ViewBag.Message = "Successfully Uploaded";
+            return View("Index");
+        }
+    }
+}
